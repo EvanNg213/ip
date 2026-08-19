662145cc5430798ad34d14cb77f03f447601a912
Evan Ng <engh3236@gmail.com>
2026-08-19 19:15:51 +0800

    Level 1 echo and bye

--- src/main/java/Chocolate.java
+++ src/main/java/Chocolate.java
@@ -1,14 +1,32 @@
-/** Starts the Chocolate chatbot application. */
+import java.util.Scanner;
+
+/** Start application. */
 public class Chocolate {
     public static void main(String[] args) {
-        String divider = "____________________________________________________________";
+        String divider = "**************************************";
         String banner = "Chocolate";
 
         System.out.println(divider);
         System.out.println(banner);
-        System.out.println("Hello! My name is Chocolate!");
-        System.out.println("How can I help you?");
+        System.out.println("Hi, my name is Chocolate!");
+        System.out.println("How may I help you today?");
         System.out.println(divider);
-        System.out.println("Thank you and Goodbye!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println(divider);
+
+            if (command.equals("bye")) {
+                System.out.println("Thank you and see you again");
+                System.out.println(divider);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(divider);
+        }
     }
 }
