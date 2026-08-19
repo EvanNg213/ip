a56ae749ffbbf99c77df909bbc6804ca89eb6acd
Evan Ng <engh3236@gmail.com>
2026-08-19 15:42:01 +0800

    Complete Level-0 greeting and farewell

--- src/main/java/Chocolate.java
+++ src/main/java/Chocolate.java
@@ -1,7 +1,14 @@
 /** Starts the Chocolate chatbot application. */
 public class Chocolate {
     public static void main(String[] args) {
-        String banner = "Chocolate\n";
+        String divider = "____________________________________________________________";
+        String banner = "Chocolate";
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I'm Chocolate.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
