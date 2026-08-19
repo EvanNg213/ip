94832b6d4d1e949c05572f0e7bb8212a9b0579e7
Evan Ng <engh3236@gmail.com>
2026-08-19 15:16:02 +0800

    Rename chatbot from Duke to Chocolate

---
+++ src/main/java/Chocolate.java
@@ -0,0 +1,7 @@
+/** Starts the Chocolate chatbot application. */
+public class Chocolate {
+    public static void main(String[] args) {
+        String banner = "Chocolate\n";
+        System.out.println(banner);
+    }
+}
